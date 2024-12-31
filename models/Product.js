fc7f3dfa3ffc233ead4cd9551e5ee31ba21a9acb
Emanuel <emanueloviedo9@gmail.com>
2024-12-31 01:22:09 -0300

    model update

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    id: { type: Number, required: false, unique: true},  // Si quieres un ID numérico
+    id: { type: Number, required: false, unique: true},  
     title: { type: String, required: true },
     price: { type: Number, required: true },
 });
 
-// Se crea el modelo basado en el esquema
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
